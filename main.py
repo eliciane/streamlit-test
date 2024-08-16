a8383976fae804a00f8e88d3917e330e466cdeaa
eliciane <elicianems@gmail.com>
2024-08-16 15:43:38 -0300

    setima versao

--- main.py
+++ main.py
@@ -44,4 +44,62 @@ colored_buble_hover_fig.update_layout(
 
 st.plotly_chart(colored_buble_hover_fig)
 
+chart_type = st.radio("Select chart type:", ('Scatter Plot', 'Line Chart', 'Bar Chart', 'Histogram',
+                                             'Box Plot', 'Pie Chart', '3D Scatter Plot'))
+
+#visualizar o relacionamento entre sepal length and sepal width, colored by spicies
+if chart_type == 'Scatter Plot':
+    fig = px.scatter(iris_df, x='sepal_length', y='sepal_width', color='species', title='Iris Scatter Plot')
+    st.plotly_chart(fig)
+
+#a linha do chart requer time-series data,
+#vamos simular uma linha do chart usando o iris dataset index as a faux time-axis
+elif chart_type == 'Line Chart':
+    iris_df_sorted=iris_df.sort_values(by='sepal_length').reset_index()
+    fig = px.line(iris_df_sorted, x=iris_df_sorted, y='sepal_length', color='species', title='Iris Sepal Length Line Chart')
+    st.plotly_chart(fig)
+
+#mostre a média sepal length de cada especies usando a bar chart
+elif chart_type == 'Bar Chart':
+    avg_sepal_length = iris_df.groupby('species')['sepal_length'].mean().reset_index()
+    fig = px.bar(avg_sepal_length, x='species', y='sepal_length', title='Average Sepal Length of Iris Species')
+    st.plotly_chart(fig)
+
+#mostrar a distribuição da sepal lengths mediante as especies
+elif chart_type == 'Histogram':
+    fig = px.histogram(iris_df, x='sepal_length', title='Sepal Length Distribution')
+    st.plotly_chart(fig)
+
+# mostrar a distribuição da sepal lengths para cada especie usando box plot
+elif chart_type == 'Box Plot':
+    fig = px.box(iris_df, x='species', y='sepal_length', title='Sepal Length by Species')
+    st.plotly_chart(fig)
+
+#mostrar a distribuição das especies no dataseet
+elif chart_type == 'Pie Chart':
+    species_count = iris_df['species'].value_counts().reset_index()
+    fig = px.pies(species_count, values='species', names='index', title='Iris Species Distribution')
+    st.plotly_chart(fig)
+
+# criar a 3D scatter plot showing the sepal length, sepal width, and petal length, colored by species
+elif chart_type == '3D Scatter Plot':
+    fig = px.scatter_3d(iris_df, x='sepal_length', y='sepal_width', z='petal_length', color='species',
+                        title='3D scatter plot of Iris Dataset')
+    st.plotly_chart(fig)
+
+import plotly.graph_objects as go
+import pandas as pd
+
+#calcular a matrix de correlação
+corr = iris_df.iloc[:, :4].corr()
+
+#Criar a heatmap
+fig= go.Figure(data=go.Heatmap(
+    z=corr,
+    x=corr.columns,
+    y=corr.columns
+))
+fig.update_layout(title='Heatmap of Iris Features Correlation')
+st.plotly_chart(fig)
+
 
